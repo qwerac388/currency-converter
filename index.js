b9ac9bf0237ad9e740325180bbf2618cf9bcd0c0
Tramy <tramy.w@gmail.com>
2023-12-07 14:20:45 +0800

    added currency Json

---
+++ index.js
@@ -0,0 +1,54 @@
+const currency = {
+  USD: {
+    symbol: "USD",
+    name: "United States Dollar		Canadian Dollar",
+  },
+  CAD: {
+    symbol: "CAD",
+    name: "Canadian Dollar",
+  },
+  EUR: {
+    symbol: "EUR",
+    name: "Euro",
+  },
+  JPY: {
+    symbol: "JPY",
+    name: "Japanese Yen",
+  },
+  CNY: {
+    symbol: "CNY",
+    name: "Chinese Yuan",
+  },
+  GBP: {
+    symbol: "GBP",
+    name: "British Pound",
+  },
+  CHF: {
+    symbol: "CHF",
+    name: "Swiss Franc",
+  },
+  AUD: {
+    symbol: "AUD",
+    name: "Australian Dollar",
+  },
+  KRW: {
+    symbol: "KRW",
+    name: "South Korean Won",
+  },
+  PLN: {
+    symbol: "PLN",
+    name: "Polish Zloty",
+  },
+  DKK: {
+    symbol: "DKK",
+    name: "Danish Krone",
+  },
+  TRY: {
+    symbol: "TRY",
+    name: "Turkish New Lira",
+  },
+  HKD: {
+    symbol: "HKD",
+    name: "Hong Kong Dollar",
+  },
+};
